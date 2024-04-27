b9852d6b3b41382555e75994c3fb92d3009a06b6
Xuetong Wu <wfyitf@gmail.com>
2024-04-28 00:35:06 +1000

    Update score calculation

--- utils/scores.py
+++ utils/scores.py
@@ -2,12 +2,8 @@ import pandas as pd
 import numpy as np
 import torch.nn.functional as F
 
-
 def cosine_similarity_torch(x, y):
     return F.cosine_similarity(x, y, dim=1).item()
 
 def cosine_similarity(x, y):
     return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
-
-
-
